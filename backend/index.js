2e9f70edd5fd8ee215b6e688670e84afb70777d2
arjuncvinod <arjuncvinod007@gmail.com>
2023-09-02 20:13:40 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express" 
+import {PORT} from "./config.js"
+
+const app = express()
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+app.listen(PORT,()=>{
+    console.log(`server started at port ${PORT}`);
+})
