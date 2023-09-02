5587df5dfe34efc97d856b9d71beef26cb4de3ed
arjuncvinod <arjuncvinod007@gmail.com>
2023-09-02 23:17:11 +0530

    /book post route added

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,36 @@
-import express from "express";
+import express, { request } from "express";
 import { PORT, mongoURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookmodel.js";
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+app.post("/book", async (req, res) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return res.status(400).send({ message: "all fields sent" });
+
+      newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        piblishyear: req.body.publishYear,
+      };
+    }
+    const book = await Book.create(newBook);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error");
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`server started at port ${PORT}`);
 });
--- backend/models/bookmodel.js
+++ backend/models/bookmodel.js
@@ -6,7 +6,7 @@ const bookSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    authour: {
+    author: {
       type: String,
       required: true,
     },
