604b60f932ece85341950567a18eee15a135d765
arjuncvinod <arjuncvinod007@gmail.com>
2023-09-02 22:14:25 +0530

    mongoose connected

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
-import express from "express" 
-import {PORT,mongoURL} from "./config.js"
+import express from "express";
+import { PORT, mongoURL } from "./config.js";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
+app.get('/', (req, res) => {
+  res.send("hello world");
+});
+app.listen(PORT, () => {
+  console.log(`server started at port ${PORT}`);
+});
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
-app.listen(PORT,()=>{
-    console.log(`server started at port ${PORT}`);
-})
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
