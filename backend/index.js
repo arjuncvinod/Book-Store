8f45bb671e40c75db1ed8ce7e5f8370a46079460
arjuncvinod <arjuncvinod007@gmail.com>
2023-09-03 22:41:05 +0530

    response organized

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,10 @@ app.post("/books", async (req, res) => {
 app.get('/books',async(req,res)=>{
   try{
     const books= await Book.find({})
-   return res.status(200).json(books)
+   return res.status(200).json({
+    count:books.length,
+    data:books
+  })
   }catch(error){
     res.status(500).send({message:error.message})
   }
