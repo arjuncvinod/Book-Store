98c0089c66a99a8235f0072bb95405e8c3e4bdbd
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-05 23:36:22 +0530

    axios installed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../componets/Spinner";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 
 function Home() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [books, setBooks] = useState("");
+  const [loader, setLoader] = useEffect("false");
+  useEffect(() => {
+    setLoader(true);
+    axios.get("localhost:8000/books").then((res) => {
+      setBooks(res.data.data);
+      setLoader(false);
+    });
+  });
+  return <div></div>;
 }
 
-export default Home
+export default Home;
