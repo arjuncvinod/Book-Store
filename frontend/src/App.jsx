96d079093dd1e731235656e714cb5aaa11ea767f
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-06 00:48:01 +0530

    books displayed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import EditBook from './pages/EditBook'
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={Home}  />
+      <Route path='/' element={<Home />}  />
       <Route path='/books/create' element={<CreateBooks />} />
       <Route path='/books/details' element={<ShowBook />}  />
       <Route path='/books/edit/:id' element={<EditBook />}  />
