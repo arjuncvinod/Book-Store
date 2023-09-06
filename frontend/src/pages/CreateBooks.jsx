3e84dfa92503d0a72e9b1556a74eb5842ba1ce22
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-06 23:34:28 +0530

    create book added

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React , { useState } from 'react'
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+const CreateBooks = () => {
+const [title, setTitle] = useState('');
+const [author, setAuthor] = useState('');
+const [publishYear, setPublishYear] = useState(' ');
+const [loading, setLoading] = useState(false);
+const navigate = useNavigate()
 
-function CreateBooks() {
+const handleSaveBook=()=>{
+ const data={
+  title,
+  author,
+  publishYear
+ }
+ setLoading(true)
+ axios.post('http://localhost:8000/books',data).then(()=>{
+  setLoading(false)
+  navigate('/')
+ }).catch((err)=>{
+  console.log(err);
+  setLoading(false)
+  alert("error")
+ })
+}
   return (
     <div>
       CreateBooks
