60c76d21b6aaf2621c7577f69a13e01c56e3a012
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-08 09:35:21 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT=8000;
+export const PORT = process.env.PORT || 3000;
 
 export const mongoURL='mongodb+srv://root:arjuncvinod@book-store.qoibc7b.mongodb.net/book-collection?retryWrites=true&w=majority'
